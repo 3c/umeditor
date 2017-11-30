4ddd779769cfc22f7072158314f7d026dd45dddb
cx <www.chenxi2008@163.com>
2017-11-30 15:28:00 +0800

    修复视频预览界面的url未替换bug

--- dialogs/video/video.js
+++ dialogs/video/video.js
@@ -98,7 +98,7 @@
                 return;
             }
             $("#eduiVideoPreview", me.$widget)[0].innerHTML = '<embed type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer"' +
-                ' src="' + url + '"' +
+                ' src="' + conUrl + '"' +
                 ' width="' + 420  + '"' +
                 ' height="' + 280  + '"' +
                 ' wmode="transparent" play="true" loop="false" menu="false" allowscriptaccess="never" allowfullscreen="true" ></embed>';
